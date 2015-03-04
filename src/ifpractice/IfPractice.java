47dbffbcd876a6ce222090024bdfddbebd964ef2
rkhatchadourian <rkhatchadourian@CST_N417_25.cst.citytech.cuny.edu>
2015-03-04 16:53:08 -0500

    Initial commit.

---
+++ src/ifpractice/IfPractice.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ifpractice;
+
+/**
+ *
+ * @author rkhatchadourian
+ */
+public class IfPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //TODO: Get y from the user.
+        int y;
+        
+        int x = 5;
+        //TODO: Set x to 0 when y is 20.
+        
+        //TODO: Output both x and y.
+    }
+    
+}
