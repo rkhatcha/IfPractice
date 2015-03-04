86fb880c4d3e52a0b81d63a9e3615668a72d38c0
rkhatchadourian <rkhatchadourian@CST_N417_25.cst.citytech.cuny.edu>
2015-03-04 17:02:59 -0500

    Finish the exercise.

--- src/ifpractice/IfPractice.java
+++ src/ifpractice/IfPractice.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ifpractice;
+
+import java.util.Scanner;
 
 /**
  *
@@ -16,13 +17,25 @@ public class IfPractice {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //TODO: Get y from the user.
+        System.out.print("Please enter an integer: ");
+
+        //Get y from the user.
         int y;
-        
+
+        Scanner input;
+        input = new Scanner(System.in);
+
+        y = input.nextInt();
+
         int x = 5;
-        //TODO: Set x to 0 when y is 20.
-        
-        //TODO: Output both x and y.
+
+        //Set x to 0 when y is 20.
+        if (y == 20) {
+            x = 0;
+        }
+
+        //Output both x and y.
+        System.out.println("x is " + x + " and y is " + y);
     }
-    
+
 }
